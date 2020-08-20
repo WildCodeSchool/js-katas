bfebbe3798227e2dd1d79cb9f391f9390efa85a5
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-08-20 20:06:23 +0200

    fix kata social-like

--- exercises/social-like.js
+++ exercises/social-like.js
@@ -10,18 +10,6 @@ Create a function `getLikes` which receives an array of names, and returns:
 */
 
 // Your code:
-function getLikes(names) {
-  if (names.length > 2) {
-    return `${names[0]} and ${names.length - 1} other people like this`;
-  }
-  if (names.length === 2) {
-    return `${names[0]} and ${names[1]} like this`;
-  }
-  if (names.length === 1) {
-    return `${names[0]} likes this`;
-  }
-  return "Be the first to like this";
-}
 
 // Begin of tests
 const assert = require("assert");
