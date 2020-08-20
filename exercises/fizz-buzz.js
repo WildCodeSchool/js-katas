25d0805d2d6ca28e2797e43dce103ef77d8533ca
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-08-20 19:50:32 +0200

    kata fizz-buzz

---
+++ exercises/fizz-buzz.js
@@ -0,0 +1,26 @@
+"use strict";
+/*
+Create a function `fizzBuzz` which takes a number as parameter, and returns :
+
+* "Fizz", if the argument is a multiple of 3
+* "Buzz" if the argument is a multiple of 5
+* "FizzBuzz", if the argument is a multiple of 3 and 5
+* the argument as a string in any other case
+
+*/
+
+// Your code:
+
+// Begin of tests
+const assert = require("assert");
+
+assert.strictEqual(typeof fizzBuzz, "function");
+assert.strictEqual(fizzBuzz(3), "Fizz");
+assert.strictEqual(fizzBuzz(9), "Fizz");
+assert.strictEqual(fizzBuzz(5), "Buzz");
+assert.strictEqual(fizzBuzz(10), "Buzz");
+assert.strictEqual(fizzBuzz(15), "FizzBuzz");
+assert.strictEqual(fizzBuzz(30), "FizzBuzz");
+assert.strictEqual(fizzBuzz(7), "7");
+assert.strictEqual(fizzBuzz(13), "13");
+// End of tests
--- exercises/hello-you.js
+++ exercises/hello-you.js
@@ -1,12 +1,13 @@
 "use strict";
 /*
-Create a function `sayHello` which takes a String as parameter and returns : Hello first name, where "first name" is replaced with the argument value, e.g :
+Create a function `sayHello` which takes a string as parameter and returns : Hello first name, where "first name" is replaced with the argument value, e.g :
 
 > Hello Barbara
 
 If the argument is empty or null, return :
 
 > Hello World
+
 */
 
 // Your code:
