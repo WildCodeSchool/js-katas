664b096530902e4cebc637a0b0fe9bba36353306
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-08-21 13:38:43 +0200

    kata duration

---
+++ exercises/duration.js
@@ -0,0 +1,24 @@
+"use strict";
+/*
+Create a function `convertTime` which converts a time formatted as "hh:mm" in a duration in minuts.
+
+If the argument is not correctly formatted, return null.
+
+* "02:30" -> 150
+* "01:45" -> 105
+* "01h45m" -> null
+
+Add you own tests.
+
+*/
+
+// TODO your code:
+
+// Begin of tests
+const assert = require("assert");
+
+assert.strictEqual(typeof convertTime, "function");
+assert.strictEqual(convertTime.length, 1);
+// TODO add your tests:
+
+// End of tests
