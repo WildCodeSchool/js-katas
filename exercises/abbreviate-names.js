a32ca348758d95da78256919b1f09ac460ad3e05
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-08-21 13:54:24 +0200

    kata abbreviate-names

---
+++ exercises/abbreviate-names.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+Create a function `abbreviate` which converts a name into initials.
+
+The output should be capital letters with a dot separating them.
+
+E.g:
+* "Alyson Hannigan" -> "A.H"
+* "Cobie Smulders" -> "C.S"
+* "Neil Patrick Harris" -> "N.P.H"
+
+Add you own tests.
+
+*/
+
+// TODO your code:
+
+// Begin of tests
+const assert = require("assert");
+
+assert.strictEqual(typeof abbreviate, "function");
+assert.strictEqual(abbreviate.length, 1);
+// TODO add your tests:
+
+// End of tests
