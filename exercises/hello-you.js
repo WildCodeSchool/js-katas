3be1719d0fb09d566510a17fb6fe18ddb7ac4d9f
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-09-24 09:29:35 +0200

    fix

--- exercises/hello-you.js
+++ exercises/hello-you.js
@@ -2,7 +2,7 @@
 Create a function `sayHello` which takes a string as parameter and returns: "Hello {first name}", where {first name} is replaced with the argument value, example:
 * sayHello("Barbara") -> "Hello Barbara"
 
-If the argument is empty or null, return "Hello World":
+If the argument is empty, null or undefined, return "Hello World":
 * sayHello(null) -> "Hello World"
 
 */
