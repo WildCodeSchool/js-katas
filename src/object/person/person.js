d50e754881f3b535006b17e0fa83d414b1fad8e8
bastienapp <29610890+bastienapp@users.noreply.github.com>
2024-01-04 10:34:33 +0100

    fixes

--- src/object/person/person.js
+++ src/object/person/person.js
@@ -2,12 +2,12 @@
 Créez une classe Person qui comporte :
 
 - Un constructeur qui initialise trois propriétés dans cet ordre : name (string), age (nombre) et weeaboo (booléen).
-- Une méthode introduce qui renvoie "My name is {name} and I am {age}", où {name} et {age} sont remplacés par leurs propriétés respectifs.
+- Une méthode introduce qui renvoie "My name is {name} and I am {age}", où {name} et {age} sont remplacés par leurs propriétés respectives.
 - Une méthode greets qui reçoit un nom (name, string) en paramètre et renvoie "Hello {name}" si weeaboo est faux, et "Ohayou {name}-chan" si weeaboo est vrai.
 
 Ensuite, créer une classe Hero qui hérite de Person :
-- Un constructeur qui initialise une propriété "power" (string).
-- Lors de l'appel de introduce, en plus de la présentation classique, le héros devra indiquer son pouvoir.
+- Un constructeur qui initialise une nouvelle propriété power (string).
+- Lors de l'appel de introduce, en plus de la présentation classique, le héros devra indiquer son pouvoir : "My superpower is {power}".
 
 Bonus : modifier la classe Person :
 
