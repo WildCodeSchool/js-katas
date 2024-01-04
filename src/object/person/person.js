d84e6d88c16d0ec760191db9e37d9e6b3019ad53
bastienapp <29610890+bastienapp@users.noreply.github.com>
2024-01-04 09:35:23 +0100

    fixes

--- src/object/person/person.js
+++ src/object/person/person.js
@@ -2,7 +2,7 @@
 Create a class `Person` which has :
 * a constructor which initialize three attributes in this order : name (string), age (number) and weeaboo (boolean)
 * a method `introduce` which returns "My name is {name} and I am {age}", where {name} and {age} are replaced with their respective attributes
-* a method `greets` which receives a name (string) as parameter and returns "Hello {name}" if `weeaboo` is false, and "Ohayo {name}-chan" if `weeaboo` is true
+* a method `greets` which receives a name (string) as parameter and returns "Hello {name}" if `weeaboo` is false, and "Ohayou {name}-chan" if `weeaboo` is true
 * a method `match` which receives a person (Person) as parameter and returns true if the current `weeaboo` and argument `weeaboo` attributes are the same
 
 Créer une classe Hero qui hérite de Person. Il sera possible de renseigne une propriété "power" lors de la construction.
